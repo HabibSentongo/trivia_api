d94a72bfde66b7300a33a46022b42fa63e3caa54
HabibSentongo <habibsentongo@gmail.com>
2020-01-29 03:39:05 +0300

    Add more tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -49,27 +49,27 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
-    def test_get_categories_with_success_response(self):
+    def test_success_response_for_get_categories(self):
         """
-            Test getting all categories with a success response
+            Test success response for getting all categories
         """
         response = self.client().get("/api/v1/categories")
         data = json.loads(response.data)
         self.assertTrue(response.status_code, 200)
         self.assertTrue(data['success'], True)
 
-    def test_get_questions_with_success_response(self):
+    def test_success_response_for_get_questions(self):
         """
-            Test getting all questions with a success response
+            Test success response for getting all questions 
         """
         response = self.client().get('/api/v1/questions')
         data = json.loads(response.data)
         self.assertTrue(response.status_code, 200)
         self.assertTrue(data['success'], True)
 
-    def test_get_questions_with_failure_response(self):
+    def test_failure_response_for_get_questions(self):
         """
-            Test getting unavailable questions from the database
+            Test retrieving questions from empty database
         """
         question = Question.query.first()
         question.delete()
@@ -78,9 +78,9 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(response.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    def test_delete_a_questions_with_success_response(self):
+    def test_success_response_for_deleting_a_question(self):
         """
-            Test deleting an existing question from the database
+            Test deleting a question that exists in the database
         """
         question_id = Question.query.first().id
         response = self.client().delete(f"/api/v1/questions/{question_id}")
@@ -89,9 +89,9 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['message'], 'Question successfully deleted')
 
-    def test_delete_a_questions_with_failure_response(self):
+    def test_failure_response_for_deleting_a_question(self):
         """
-            Test deleting an unexisting question from the database
+            Test deleting a question that doesnot exist in the database
         """
         question_id = Question.query.first().id
         response = self.client().delete(f"/api/v1/questions/{question_id + 1}")
@@ -100,9 +100,9 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Resource not found')
 
-    def test_get_questions_of_given_category_with_success_response(self):
+    def test_success_response_for_getting_questions_of_given_category(self):
         """
-            Test getting all questions of a given category id
+            Test getting all questions of a given category
         """
         category_id = Category.query.first().id
         response = self.client().get(f"/api/v1/categories/{category_id}/questions")
@@ -111,9 +111,9 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['total_questions'], 1)
 
-    def test_get_questions_of_given_category_with_failure_response(self):
+    def test_failure_response_for_getting_questions_of_given_category(self):
         """
-            Test getting all questions of an unexisting category
+            Test getting all questions of a category that doesnot exist
         """
         category_id = Category.query.first().id
         response = self.client().get(f"/api/v1/categories/{category_id + 1}/questions")
@@ -122,16 +122,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Resource not found')
 
-    def test_create_a_question_with_success_response(self):
+    def test_successfully_adding_a_question_to_database(self):
         """
-            Test creating a question with all required fields and required request method
+            Test creating a valid question in the database
         """
         response = self.client().post('/api/v1/questions',
             content_type='application/json',
             data=json.dumps(
             {
-                'question': 'Test question',
-                'answer': 'Test answer',
+                'question': 'Is computer Science good?',
+                'answer': 'Yes its good',
                 'difficulty': 2,
                 'category': 2
             }
@@ -142,7 +142,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['message'], 'Question successfully added')
 
-    def test_create_a_question_with_failure_response(self):
+    def test_adding_a_question_with_invalid_url(self):
         """
             Test creating a question with a wrong request url
         """
@@ -150,8 +150,8 @@ class TriviaTestCase(unittest.TestCase):
             content_type='application/json',
             data=json.dumps(
             {
-                'question': 'Test question',
-                'answer': 'Test answer',
+                'question': 'Is computer Science good?',
+                'answer': 'Yes its good',
                 'difficulty': 2,
                 'category': 2
             }
@@ -162,7 +162,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Method not allowed')
 
-    def test_search_a_question_with_success_response(self):
+    def test_success_response_for_searching_a_question_in_the_database(self):
         """
             Test searching a question using a correct search term.
         """
@@ -176,7 +176,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['total_questions'], 1)
 
-    def test_search_a_question_with_failure_response(self):
+    def test_failure_response_for_searching_a_question_not_in_the_database(self):
         """
             Test searching a question using a wrong search term.
         """
@@ -190,7 +190,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Resource not found')
 
-    def test_get_quiz_question_with_success_response(self):
+    def test_success_response_for_playing_quiz_game(self):
         """
             Test playing a quiz category and previous question parameters.
         """
@@ -205,7 +205,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question'])
 
-    def test_get_quiz_question_with_failure_response(self):
+    def test_failure_response_for_playing_quiz_game(self):
         """
             Test playing a quiz category a wrong request method.
         """
